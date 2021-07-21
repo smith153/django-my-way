3243d980d9f3c03a76985aa789f17e2f6c22818c
Samuel Smith <leon36@gmail.com>
2021-07-20 23:58:05 -0500

    Update test config

--- tests/test_bare.sh
+++ tests/test_bare.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-# this is a very simple script that tests the docker configuration for cookiecutter-django
-# it is meant to be run from the root directory of the repository, eg:
-# sh tests/test_docker.sh
 
 set -o errexit
 
+# Install modern pip with new resolver:
+pip install 'pip>=20.3'
+
 # install test requirements
 pip install -r requirements.txt
+
 
 # create a cache directory
 mkdir -p .cache/bare
@@ -20,6 +21,9 @@ cd project_name
 # Install Python deps
 pip install -r requirements/local.txt
 
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics || { echo "ERROR: Generated project has syntax errors"; exit 1; }
+
 # run the project's tests
 echo "Running pytest:"
 pytest || { echo "ERROR: Pytest report some issues"; exit 1; }
@@ -27,3 +31,4 @@ pytest || { echo "ERROR: Pytest report some issues"; exit 1; }
 echo "Checking missing migrations:"
 # return non-zero status code if there are migrations that have not been created
 python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
+
